ee8bdf5b893ce687f9fd2ca5ab8ec632c3801be2
Jakub Uryga <urygajakub@gmail.com>
2024-11-09 21:48:58 +0100

    implementation of few functions

--- app.js
+++ app.js
@@ -15,24 +15,42 @@ async function loginUser(librusClient) {
 	let login = '9065521u'; //await askQuestion("Enter your librus login: ");
 	let password = 'Kuba2007'; //await askQuestion("Enter your librus password: ");
 
-	console.log('Logging user with login: ', login);
+	console.log(`Logging user with login: ${login}`);
 
 	try {
 		await librusClient.authorize(login, password);
 
-		console.log('Logged in successfully');
+		console.log('Logged in successfully\n');
+
+		accountInfo = await librusClient.info.getAccountInfo();
+		accountInfo = accountInfo.student;
+
+		console.log('User info:');
+		console.log(`> Name: ${accountInfo.nameSurname}`);
+		console.log(`> Class: ${accountInfo.class}`);
+		console.log(`> Index in the diary: ${accountInfo.index}`);
+		console.log(`> Educator: ${accountInfo.educator}\n`);
 	} catch (error) {
 		if (error.response) {
 			console.error(`Error logging in: ${error.response.status} - ${error.response.data}`);
+			process.exit(1);
 		} else if (error.request) {
 			console.error('No response received from Librus:', error.request);
+			process.exit(1);
 		} else {
 			console.error('Error during login:', error.message);
+			process.exit(1);
 		}
 	}
 }
 
-async function getAbsences(librusClient) {
+async function getLuckyNumber(librusClient) {
+	let luckyNumber = await librusClient.info.getLuckyNumber();
+
+	console.log(`Lucky number for today is: ${luckyNumber}`);
+}
+
+async function getAbsences(librusClient, month) {
 	let absences = await librusClient.absence.getAbsences();
 	absences = absences['0'];
 
@@ -40,10 +58,14 @@ async function getAbsences(librusClient) {
 		console.log('No absences to display. Good for you!');
 	} else {
 		for (let day of absences) {
+			if (parseInt(day.date.split('-')[1]) != month) {
+				continue;
+			}
+
 			console.log(`Attendance from: ${day.date}`);
 			console.log();
 
-			for (let [index, row] of day.table.entries()) {
+			for (let row of day.table.entries()) {
 				if (row != null) {
 					let absence_details = await librusClient.absence.getAbsence(row.id);
 
@@ -65,7 +87,50 @@ async function getAbsences(librusClient) {
 	}
 }
 
-async function menu() {}
+async function getAnnouncements(librusClient) {
+	let announcements = await librusClient.inbox.listAnnouncements();
+
+	if (announcements.length == 0) {
+		console.log('No announcements to display.');
+	} else {
+		announcements.forEach((announcement, index) => {
+			console.log(`ANNOUNCEMENT no. ${index}`);
+
+			console.log(`Title: ${announcement.title}`);
+			console.log(`Content:`);
+			console.log(`"${announcement.content}"`);
+			console.log(`Added by: ${announcement.user} at ${announcement.date}`);
+			console.log('\n-------------------------------------------------\n');
+		});
+
+		await askQuestion('Press Enter to continue..');
+		console.clear();
+	}
+}
+
+async function getGrades(librusClient) {
+	let grades = await librusClient.info.getGrades();
+
+	for (let subject of grades) {
+		console.log(`${subject.name.toUpperCase()}:`);
+
+		for (let [index, semester] of subject.semester.entries()) {
+			console.log(`\tSemester no. ${index + 1}:`);
+
+			process.stdout.write(`\t[`);
+
+			if (semester.grades && Array.isArray(semester.grades)) {
+				for (let grade of semester.grades) {
+					process.stdout.write(`${grade.value}, `);
+				}
+			} else {
+				console.log('No grades available.');
+			}
+
+			process.stdout.write(`]\n`);
+		}
+	}
+}
 
 async function main() {
 	let client = new Librus();
@@ -73,33 +138,41 @@ async function main() {
 	try {
 		await loginUser(client);
 
-		console.log('Available options: \n1. List Announcements \n2. List Homework \n3. List Absences \n4. Get Timetable');
+		console.log('Available options: \n1. List Announcements \n2. Get Lucky Number \n3. List Absences \n4. Get Grades');
 		const option = await askQuestion('Choose option: ');
+		console.log();
 
 		switch (option) {
 			case '1':
 				try {
-					const announcements = await client.inbox.listAnnouncements();
+					await getAnnouncements(client);
 				} catch (error) {
 					console.error('Error fetching announcements:', error);
 				}
 				break;
 			case '2':
-				// Placeholder for Homework option
+				try {
+					await getLuckyNumber(client);
+				} catch (error) {
+					console.error('Error fetching lucky number:', error);
+				}
 				break;
 			case '3':
 				try {
-                    await getAbsences(client);
+					month = await askQuestion('Select the month whose absence you want to get (1-12): ');
+					if (!Number.isInteger(Number(month)) || month < 1 || month > 12) {
+						throw new Error('Provided input is not valid month of the year.');
+					}
+					await getAbsences(client, month);
 				} catch (error) {
 					console.error('Error fetching absences:', error);
 				}
 				break;
 			case '4':
 				try {
-					const timetable = await client.calendar.getCalendar();
-					console.log(timetable);
+					await getGrades(client);
 				} catch (error) {
-					console.error('Error fetching timetable:', error);
+					console.error('Error fetching grades:', error);
 				}
 				break;
 			default:
